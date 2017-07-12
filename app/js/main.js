08ca1de347ac2a61bbea52a4f5e5891767c13d15
Henrik <henkedanne@gmail.com>
2017-07-12 21:54:47 +0200

    shows correct info

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,7 @@ let dataHolder1 = document.getElementById('data-holder1');
 let dataHolder2 = document.getElementById('data-holder2');
 
 let apiKey = '18a7570569054c7cae4f437c57c88ade';
-let timeWindow = 10;
+let timeWindow = 30;
 let siteId = 9293;
 let url = 'https://cors.now.sh/' + 'http://api.sl.se/api2/realtimedeparturesV4.json?key=' + apiKey + '&siteid=' + siteId + '&timewindow=' + timeWindow;
 
@@ -16,19 +16,24 @@ function getData() {
             console.log(data);
             let testData = data.ResponseData.Metros;
             
-            let stationNorsborg = testData.filter(function(station) {
-                if (station.Destination = 'Norsborg') {
-                    return station;
-                }
+            let norsborg = testData.filter(function (el) {
+                return el.JourneyDirection == 2;
             });
-            console.log(stationNorsborg);
+            let ropsten = testData.filter(function (el) {
+                return el.JourneyDirection == 1;
+            });
+            
 
-            destName1.innerHTML = stationNorsborg["0"].Destination;
-            dataHolder1.innerHTML = stationNorsborg["0"].DisplayTime;
-
+            destName1.innerHTML = norsborg["0"].Destination;
+            dataHolder1.innerHTML = norsborg["0"].DisplayTime;
+           
+            destName2.innerHTML = ropsten["0"].Destination;
+            dataHolder2.innerHTML = ropsten["0"].DisplayTime;
         });
 }
+    
+getData();
+setInterval(getData, 15000);
 
-// @TODO filter functionen verkar lite skum. den hoppar lite emellan olika arrays,
-// hitta ett sätt att få rätt information
-// på rätt plats. 
+// @TODO filter functionen verkar fungera bättre nu.
+// frågan är om detta är bästa metoden för att få ut den?
