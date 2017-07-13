c6a5545c94975cfe82070ccf04994c1b86ddd673
Henrik <henkedanne@gmail.com>
2017-07-13 08:57:00 +0200

    new structure and added next

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,7 @@ let destName2 = document.getElementById('dest-name2');
 let dataHolder1 = document.getElementById('data-holder1');
 let dataHolder2 = document.getElementById('data-holder2');
 
-let apiKey = '18a7570569054c7cae4f437c57c88ade';
+const apiKey = '18a7570569054c7cae4f437c57c88ade';
 let timeWindow = 30;
 let siteId = 9293;
 let url = 'https://cors.now.sh/' + 'http://api.sl.se/api2/realtimedeparturesV4.json?key=' + apiKey + '&siteid=' + siteId + '&timewindow=' + timeWindow;
@@ -11,7 +11,7 @@ let url = 'https://cors.now.sh/' + 'http://api.sl.se/api2/realtimedeparturesV4.j
 
 function getData() {
    fetch(url)
-        .then(response => response.json())
+        .then(response => response.json())  
         .then(data => {
             console.log(data);
             let testData = data.ResponseData.Metros;
@@ -24,16 +24,16 @@ function getData() {
             });
             
 
-            destName1.innerHTML = norsborg["0"].Destination;
-            dataHolder1.innerHTML = norsborg["0"].DisplayTime;
+            destName1.innerHTML = norsborg["0"].Destination + ' ' + norsborg["0"].DisplayTime;
+            dataHolder1.innerHTML = 'Nästa: ' +  norsborg["1"].DisplayTime;
            
-            destName2.innerHTML = ropsten["0"].Destination;
-            dataHolder2.innerHTML = ropsten["0"].DisplayTime;
+            destName2.innerHTML = ropsten["0"].Destination + ' ' + ropsten["0"].DisplayTime;
+            dataHolder2.innerHTML = 'Nästa: ' + ropsten["1"].DisplayTime;
         });
 }
     
 getData();
-setInterval(getData, 15000);
+setInterval(getData, 30000);
 
 // @TODO filter functionen verkar fungera bättre nu.
 // frågan är om detta är bästa metoden för att få ut den?
